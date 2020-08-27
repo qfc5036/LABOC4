ad3015870777be69102371d3a2d85de19548cf92
qfc5036 <qfc5036@psu.edu>
2020-08-27 02:02:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+name =input("Enter your name: ")
+print("Hello, " + name +"!")
+age = input("Enter your age: ")
+age = int(age)
+print(age + 1)
+
+
+print("You will be " + str(age + 1 ) + "next year")
